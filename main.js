e873dce709bcfdb4e6593777b5410c86fb151bcf
TheJLifeX <44620382+TheJLifeX@users.noreply.github.com>
2019-03-02 21:44:49 +0100

    Implement inputs validation whilst adding column and adding row.

--- main.js
+++ main.js
@@ -16,6 +16,12 @@ window.onload = function () {
 
             // add row
             $("#add-row").on("click", function (event) {
+                const inputs = document.querySelectorAll('.create-row input');
+                for (let i = 0; i < inputs.length; i++) {
+                    if (!inputs[i].checkValidity()) {
+                        return;
+                    }
+                }
                 event.preventDefault();
                 $('.fas.fa-check').css('visibility', 'visible');
                 const newRow = [];
@@ -27,11 +33,18 @@ window.onload = function () {
 
                 window.setTimeout(() => {
                     $('.fas.fa-check').css('visibility', 'hidden');
-                }, 1000);
+                }, 500);
             });
 
             // edit row
             $('#edit-row-confirm').on('click', function () {
+                const inputs = document.querySelectorAll('.edit-row-modal input');
+                for (let i = 0; i < inputs.length; i++) {
+                    if (!inputs[i].checkValidity()) {
+                        return;
+                    }
+                }
+                event.preventDefault();
                 const editedRow = [];
                 document.querySelectorAll(".edit-row-modal .form-control").forEach(function (element) {
                     editedRow.push(element.value);
@@ -41,7 +54,7 @@ window.onload = function () {
                 $('.edit-row-modal').modal('hide');
             });
 
-
+            // 
             $('#create-column-input02').on('change', function () {
                 $('.constrains').css('display', 'none');
                 const selectValue = $(this).val();
@@ -66,33 +79,72 @@ window.onload = function () {
 
             $('#column-actions').on('mouseleave', function () {
                 $('#column-actions').stop().fadeTo(300, 0);
+                $('#column-actions').hide();
             });
-            // end
+
 
             // show the add column modal
             $('#icon-add-column').on('click', function () {
                 $('.add-column-modal').modal('show');
             });
 
+            // add column validation of pattern
+            $('#type02-input02, #type03-input02').on('keyup', function () {
+                $(this)[0].setCustomValidity('');
+                try {
+                    new RegExp($(this).val());
+                } catch (e) {
+                    $(this)[0].setCustomValidity('Invalid regular expression')
+                }
+            });
+
             // add column
             $('#add-column').on('click', function () {
+                if (!$('#create-column-input01')[0].checkValidity()) {
+                    return;
+                }
+
                 const inputMetaDataObj = {};
                 inputMetaDataObj.name = $('#create-column-input01').val();
                 inputMetaDataObj.type = $('#create-column-input02').val();
                 if (inputMetaDataObj.type === 'drop-down') {
+                    const inputs = document.querySelectorAll('[id*="type01"]');
+                    for (let i = 0; i < inputs.length; i++) {
+                        if (!inputs[i].checkValidity()) {
+                            return;
+                        }
+                    }
                     inputMetaDataObj.optionsArray = $('#type01-input01').val().split(',');
                     inputMetaDataObj.defaultValue = $('#type01-input02').val();
                 } else if (inputMetaDataObj.type === 'text') {
+                    const inputs = document.querySelectorAll('[id*="type02"]');
+                    for (let i = 0; i < inputs.length; i++) {
+                        if (!inputs[i].checkValidity()) {
+                            return;
+                        }
+                    }
                     inputMetaDataObj.placeholder = $('#type02-input01').val();
                     inputMetaDataObj.pattern = $('#type02-input02').val();
                     inputMetaDataObj.defaultValue = $('#type02-input03').val();
                 } else if (inputMetaDataObj.type === 'number') {
+                    const inputs = document.querySelectorAll('[id*="type03"]');
+                    for (let i = 0; i < inputs.length; i++) {
+                        if (!inputs[i].checkValidity()) {
+                            return;
+                        }
+                    }
                     inputMetaDataObj.placeholder = $('#type03-input01').val();
                     inputMetaDataObj.pattern = $('#type03-input02').val();
                     inputMetaDataObj.minValue = $('#type03-input03').val();
                     inputMetaDataObj.maxValue = $('#type03-input04').val();
                     inputMetaDataObj.defaultValue = $('#type03-input05').val();
                 } else if (inputMetaDataObj.type === 'date') {
+                    const inputs = document.querySelectorAll('[id*="type04"]');
+                    for (let i = 0; i < inputs.length; i++) {
+                        if (!inputs[i].checkValidity()) {
+                            return;
+                        }
+                    }
                     // TODO
                 }
                 inputsMetaData.splice(currentColumnIndex, 0, inputMetaDataObj);
@@ -127,6 +179,10 @@ window.onload = function () {
                 $('#table_id tbody').remove();
                 initDataTable();
             });
+
+
+            // check inputs validations for add column modal
+            $('#add-column-modal .form-control')
         });
     }
 }
@@ -207,15 +263,15 @@ function createInput(inputMetaDataObj) {
         let temp = '';
         inputMetaDataObj.optionsArray.forEach(function (value, index) {
             temp += '<option value="' + value + '"';
-            index === 0 ? temp += 'selected>' : temp += '>';
+            (value === inputMetaDataObj.defaultValue) ? temp += 'selected>' : temp += '>';
             temp += value + '</option>';
         });
         result = '<div class="col-md-2 mb-3"><label>' + inputMetaDataObj.name + '</label> <select class="form-control">' + temp + '</select> </div>';
     } else if (inputMetaDataObj.type === 'text') {
-        result = '<div class="col-md-2 mb-3"><label>' + inputMetaDataObj.name + '</label><input type="text" class="form-control" placeholder="' + inputMetaDataObj.placeholder + '" pattern="' + inputMetaDataObj.pattern + '"></div>';
+        result = '<div class="col-md-2 mb-3"><label>' + inputMetaDataObj.name + '</label><input type="text" class="form-control" placeholder="' + inputMetaDataObj.placeholder + '" pattern="' + inputMetaDataObj.pattern + '" required></div>';
     } else if (inputMetaDataObj.type === 'number') {
         inputMetaDataObj.placeholder = $('#type03-input01').val();
-        result = '<div class="col-md-2 mb-3"><label>' + inputMetaDataObj.name + '</label><input type="number" class="form-control" placeholder="' + inputMetaDataObj.placeholder + '" pattern="' + inputMetaDataObj.pattern + '" min="' + inputMetaDataObj.minValue + '" max="' + inputMetaDataObj.maxValue + '"></div>';
+        result = '<div class="col-md-2 mb-3"><label>' + inputMetaDataObj.name + '</label><input type="number" class="form-control" placeholder="' + inputMetaDataObj.placeholder + '" pattern="' + inputMetaDataObj.pattern + '" min="' + inputMetaDataObj.minValue + '" max="' + inputMetaDataObj.maxValue + '" required></div>';
     } else if (inputMetaDataObj.type === 'date') {
         // TODO
     }
